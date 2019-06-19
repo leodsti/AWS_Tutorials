5074455bd83b3ced17e932ddf52f938b6ab95b76
leodsti <33124065+leodsti@users.noreply.github.com>
2019-06-19 16:36:24 +0200

    A little change to the read function

--- MNIST/keras_flask.py
+++ MNIST/keras_flask.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
-from scipy.misc import imread, imresize, imsave
+from imageio import imread
+from PIL import Image
+from keras.preprocessing import image
 import sys
 import os
 import re
@@ -8,7 +10,6 @@ import base64
 from keras.models import load_model
 import tensorflow as tf
 import numpy as np
-import cv2
 
 app = Flask(__name__)
 
@@ -17,13 +18,9 @@ sys.path.append(os.path.abspath("./cnn-mnist"))
 #Initialize some global variables
 
 
-    
-
 global model, graph
 model = load_model('./cnn-mnist')
 graph = graph = tf.get_default_graph()
-
-
 
 @app.route('/')
 def index():
@@ -36,22 +33,15 @@ def convertImage(imgData1):
     
 def loadImage(filename):
         img_rows = img_cols = 28
-        img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+        img = image.load_img(filename,grayscale=True)
         img = imresize(img, (28, 28))
+        img = image.img_to_array(img)
         img = img / 255
         # Reshape from (28,28) to (1,28,28,1) : 1 sample, 28x28 pixels, 1 channel (B/W)
         img = np.expand_dims(img, axis=0)
         img = np.expand_dims(img, axis=0)
         img = np.reshape(img, (1,img_cols,img_rows,1))
         return np.array(img)
-    
-    
-for digit in range(0,10):
-    img = "9.png"
-    img = loadImage(img)
-    classes = model.predict(img)
-    predicted = np.argmax(classes)
-    print(predicted)
     
 @app.route('/predict/', methods=['GET', 'POST'])
 def predict():
